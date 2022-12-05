376b54d7df8430dd83ed4834fb73264b9abb16ed
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-05 17:56:12 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ if __name__ == "__main__" :
     plugins = dict(root="plugins")
     Warrior = Clinton(
         "@UPLOADER_X_BOT",
-        bot_token=Config.TG_BOT_TOKEN,
+        bot_token=Config.BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins)
--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
-    # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -16,9 +14,8 @@ class Config(object):
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    CHUNK_SIZE = int(128)
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -26,14 +23,11 @@ class Config(object):
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = int(os.environ.get("AUTH_USERS", "1684438752"))
     # database uri (mongodb)
+    DATABASE_NAME = "JOSProjects"
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    PORT = os.environ.get("PORT", "8080")
+    MAX_RESULTS = "50"
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-100")
