c31b723bbdbc225dd7ba701d7b8b17aaa7a1d335
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-05 18:12:23 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ if __name__ == "__main__" :
     Warrior = Clinton(
         "@UPLOADER_X_BOT",
         bot_token=Config.BOT_TOKEN,
-        api_id=Config.APP_ID,
+        api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins)
     Warrior.run()
